26856c10daba5aaf6c2f49720fd29287ce35fc5a
Anlanther <anlanther@gmail.com>
2024-09-05 08:43:39 +0800

    feat(app): moved strapi key to constant file

--- calendar-app/src/app/services/data.service.ts
+++ calendar-app/src/app/services/data.service.ts
@@ -1,14 +1,13 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map } from 'rxjs';
+import { KEYS } from '../constants/keys.constant';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DataService {
-  // generated in Strapi
-  private apiKey =
-    '795b3af756282b1fc8197e8d17355de9528d2ae8b03292020c95840a22268bbc31a9cf9466296611a6a23f48fcd55dea8a6303fb399a60a46b1ca2f071faaf1f3dc47a6c4e500b257c9e54c6fa625a6d6549b9ece7a9e1f81c6e410b71c55eb9f06465d6f56ffa21efabcd4c88767c7ec726a511238e9afcb85781b33bbb78e3';
+  private apiKey = KEYS.STRAPI_KEY;
   private apiUrl = 'http://localhost:1337/graphql';
 
   private httpOptions = {
