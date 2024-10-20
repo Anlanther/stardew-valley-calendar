84543e96ad3c395ddb3ac766950fa6bfc1dba05a
Anlanther <anlanther@gmail.com>
2024-10-20 23:36:25 +0800

    tests(app): added unit tests to state

--- calendar-app/src/app/state/app.effect.spec.ts
+++ calendar-app/src/app/state/app.effect.spec.ts
@@ -8,45 +8,23 @@ import { Action } from '@ngrx/store';
 import { MockStore, provideMockStore } from '@ngrx/store/testing';
 import { cold, hot } from 'jest-marbles';
 import { Observable, of, throwError } from 'rxjs';
+import { MOCK_CALENDARS } from '../../../tests/utils/mocks/calendars.mock';
 import { AppStore } from '../models/app-store.model';
-import { Story } from '../models/story.model';
-import { User } from '../models/user.model';
-import { LoggerService } from '../services/logger/logger.service';
-import { StoryDataService } from '../services/story/story-data.service';
-import { UserDataService } from '../services/user/user-data.service';
+import { CalendarDataService } from '../services/calendar/calendar-data.service';
+import { DataService } from '../services/data.service';
+import { GameEventDataService } from '../services/game-event/game-event-data.service';
+import { OfflineDataService } from '../services/offline-data.service';
 import { AppActions } from './app.actions';
 import { AppEffects } from './app.effect';
 import { AppFeature } from './app.state';
 
 describe('AppEffects', () => {
-  const mockUsers: User[] = [
-    {
-      name: 'John',
-      favouriteStories: [{ title: 'Matilda', author: 'Roald Dahl' }],
-    },
-    {
-      name: 'Jane',
-      favouriteStories: [{ title: 'Fantastic Mr. Fox', author: 'Roald Dahl' }],
-    },
-  ];
-  const mockStories: Story[] = [
-    {
-      title: 'Story1',
-      author: 'A1',
-    },
-    {
-      title: 'Story2',
-      author: 'A2',
-    },
-    { title: 'Matilda', author: 'Roald Dahl' },
-    { title: 'Fantastic Mr. Fox', author: 'Roald Dahl' },
-  ];
-
   let spectator: SpectatorService<AppEffects>;
   let mockActions$ = new Observable<Action>();
-  let mockUserDataService: SpyObject<UserDataService>;
-  let mockStoryDataService: SpyObject<StoryDataService>;
-  let mockLoggerService: SpyObject<LoggerService>;
+  let mockCalendarDataService: SpyObject<CalendarDataService>;
+  let mockGameEventDataService: SpyObject<GameEventDataService>;
+  let mockDataService: SpyObject<DataService>;
+  let mockOfflineDataService: SpyObject<OfflineDataService>;
   let mockStore: MockStore<AppStore>;
 
   const createService = createServiceFactory({
@@ -54,101 +32,177 @@ describe('AppEffects', () => {
     providers: [
       provideMockActions(() => mockActions$),
       provideMockStore({
-        selectors: [
-          { selector: AppFeature.selectActiveUser, value: mockUsers[0] },
-        ],
+        selectors: [],
       }),
     ],
-    mocks: [UserDataService, StoryDataService, LoggerService],
+    mocks: [
+      CalendarDataService,
+      GameEventDataService,
+      DataService,
+      OfflineDataService,
+    ],
   });
 
   beforeEach(() => {
     spectator = createService();
-    mockUserDataService = spectator.inject(UserDataService);
-    mockStoryDataService = spectator.inject(StoryDataService);
-    mockLoggerService = spectator.inject(LoggerService);
+    mockCalendarDataService = spectator.inject(CalendarDataService);
+    mockGameEventDataService = spectator.inject(GameEventDataService);
+    mockDataService = spectator.inject(DataService);
+    mockOfflineDataService = spectator.inject(OfflineDataService);
     mockStore = spectator.inject(MockStore);
   });
 
-  describe('loadApp$', () => {
-    it('should trigger getUsers and getStories action', () => {
-      mockActions$ = hot('-a', { a: AppActions.loadApp() });
+  describe('initialise$', () => {
+    it('should trigger get calendar and create default events action when the app calls to initialise', () => {
+      mockActions$ = hot('-a', { a: AppActions.initialise() });
       const expected = cold('-(ab)', {
-        a: AppActions.getUsers(),
-        b: AppActions.getStories(),
+        a: AppActions.getCalendars(),
+        b: AppActions.createDefaultGameEvents(),
       });
 
-      expect(spectator.service.loadApp$).toBeObservable(expected);
+      expect(spectator.service.initialise$).toBeObservable(expected);
+    });
+    it('should trigger get calendar and create default events action when the app is set to offline mode', () => {
+      mockActions$ = hot('-a', { a: AppActions.setOfflineMode() });
+      const expected = cold('-(ab)', {
+        a: AppActions.getCalendars(),
+        b: AppActions.createDefaultGameEvents(),
+      });
+
+      expect(spectator.service.initialise$).toBeObservable(expected);
     });
   });
 
-  describe('getUsers$', () => {
-    it('should call on the userDataService to get a list of users and trigger getUsersSuccess action', () => {
-      const storeSpy = jest.spyOn(mockStore, 'dispatch');
+  describe('getAllCalendars$', () => {
+    it('should call on getCalendarsSuccess with a list of calendars obtained from calendar data service when the app is not on offline mode', () => {
+      const mockCalendarsResponseFromApi = MOCK_CALENDARS;
+      mockStore.overrideSelector(AppFeature.selectOfflineMode, false);
 
-      mockActions$ = hot('-a', { a: AppActions.getUsers() });
       const expected = cold('-a', {
-        a: AppActions.getUsersSuccess(mockUsers),
+        a: AppActions.getCalendarsSuccess(mockCalendarsResponseFromApi),
       });
-      mockUserDataService.getUsers.mockReturnValue(of(mockUsers));
-
-      expect(spectator.service.getUsers$).toBeObservable(expected);
-      spectator.service.getUsers$.subscribe(() => {
-        expect(mockUserDataService.getUsers()).toHaveBeenCalled();
-        expect(storeSpy).toHaveBeenCalledWith(AppActions.loadComplete());
-      });
-    });
-    it('should trigger getUsersFailure action if the call to the userDataService fails', () => {
-      const mockError = { message: 'Error' };
-      mockUserDataService.getUsers.mockReturnValue(throwError(() => mockError));
-      mockActions$ = hot('-a', { a: AppActions.getUsers() });
-      const expected = cold('-(a|)', {
-        a: AppActions.getUsersFailure(mockError.message),
-      });
-
-      expect(spectator.service.getUsers$).toBeObservable(expected);
-    });
-  });
-
-  describe('getStories$', () => {
-    it('should call on the storyDataService to get a list of users and trigger getStoriesSuccess action', () => {
-      mockActions$ = hot('-a', { a: AppActions.getStories() });
-      const expected = cold('-a', {
-        a: AppActions.getStoriesSuccess(mockStories),
-      });
-      mockStoryDataService.getStories.mockReturnValue(of(mockStories));
-
-      expect(spectator.service.getStories$).toBeObservable(expected);
-      spectator.service.getStories$.subscribe(() => {
-        expect(mockStoryDataService.getStories()).toHaveBeenCalled();
-      });
-    });
-    it('should trigger getStoriesFailure action if the call to the userDataService fails', () => {
-      const mockError = { message: 'Error' };
-      mockStoryDataService.getStories.mockReturnValue(
-        throwError(() => mockError)
+      mockCalendarDataService.getAll.mockReturnValue(
+        of(mockCalendarsResponseFromApi),
       );
-      mockActions$ = hot('-a', { a: AppActions.getStories() });
+
+      expect(spectator.service.getAllCalendars$).toBeObservable(expected);
+      spectator.service.getAllCalendars$.subscribe(() => {
+        expect(mockCalendarDataService.getAll()).toHaveBeenCalled();
+      });
+
+      mockActions$ = hot('-a', { a: AppActions.getCalendars() });
+    });
+    it('should call on getCalendarsSuccess with a list of calendars obtained from offline data service when the app is on offline mode', () => {
+      const mockCalendarsResponseFromApi = MOCK_CALENDARS;
+      mockStore.overrideSelector(AppFeature.selectOfflineMode, true);
+
+      const expected = cold('-a', {
+        a: AppActions.getCalendarsSuccess(mockCalendarsResponseFromApi),
+      });
+      mockOfflineDataService.getAllCalendars.mockReturnValue(
+        of(mockCalendarsResponseFromApi),
+      );
+
+      expect(spectator.service.getAllCalendars$).toBeObservable(expected);
+      spectator.service.getAllCalendars$.subscribe(() => {
+        expect(mockOfflineDataService.getAllCalendars()).toHaveBeenCalled();
+      });
+
+      mockActions$ = hot('-a', { a: AppActions.getCalendars() });
+    });
+    it('should trigger aPIFailed action when calendar data service fails', () => {
+      const mockError = { message: 'Error' };
+      mockCalendarDataService.getAll.mockReturnValue(
+        throwError(() => mockError),
+      );
+      mockActions$ = hot('-a', { a: AppActions.getCalendars() });
       const expected = cold('-(a|)', {
-        a: AppActions.getStoriesFailure(mockError.message),
+        a: AppActions.aPIFailed(),
       });
 
-      expect(spectator.service.getStories$).toBeObservable(expected);
+      expect(spectator.service.getAllCalendars$).toBeObservable(expected);
     });
   });
 
-  describe('notification$', () => {
-    it('should call on the logService to log user and action', () => {
-      const expected = { user: null, action: AppActions.loadApp };
-      spectator.service.notification$.subscribe(() => {
-        expect(mockLoggerService.log()).toHaveBeenCalledWith(
-          expected.user,
-          expected.action
-        );
-      });
+  // describe('loadApp$', () => {
+  //   it('should trigger getUsers and getStories action', () => {
+  //     mockActions$ = hot('-a', { a: AppActions.loadApp() });
+  //     const expected = cold('-(ab)', {
+  //       a: AppActions.getUsers(),
+  //       b: AppActions.getStories(),
+  //     });
 
-      mockStore.overrideSelector(AppFeature.selectActiveUser, null);
-      mockActions$ = hot('-a', { a: expected.action() });
-    });
-  });
+  //     expect(spectator.service.loadApp$).toBeObservable(expected);
+  //   });
+  // });
+
+  // describe('getUsers$', () => {
+  //   it('should call on the userDataService to get a list of users and trigger getUsersSuccess action', () => {
+  //     const storeSpy = jest.spyOn(mockStore, 'dispatch');
+
+  //     mockActions$ = hot('-a', { a: AppActions.getUsers() });
+  //     const expected = cold('-a', {
+  //       a: AppActions.getUsersSuccess(mockUsers),
+  //     });
+  //     mockUserDataService.getUsers.mockReturnValue(of(mockUsers));
+
+  //     expect(spectator.service.getUsers$).toBeObservable(expected);
+  //     spectator.service.getUsers$.subscribe(() => {
+  //       expect(mockUserDataService.getUsers()).toHaveBeenCalled();
+  //       expect(storeSpy).toHaveBeenCalledWith(AppActions.loadComplete());
+  //     });
+  //   });
+  //   it('should trigger getUsersFailure action if the call to the userDataService fails', () => {
+  //     const mockError = { message: 'Error' };
+  //     mockUserDataService.getUsers.mockReturnValue(throwError(() => mockError));
+  //     mockActions$ = hot('-a', { a: AppActions.getUsers() });
+  //     const expected = cold('-(a|)', {
+  //       a: AppActions.getUsersFailure(mockError.message),
+  //     });
+
+  //     expect(spectator.service.getUsers$).toBeObservable(expected);
+  //   });
+  // });
+
+  // describe('getStories$', () => {
+  //   it('should call on the storyDataService to get a list of users and trigger getStoriesSuccess action', () => {
+  //     mockActions$ = hot('-a', { a: AppActions.getStories() });
+  //     const expected = cold('-a', {
+  //       a: AppActions.getStoriesSuccess(mockStories),
+  //     });
+  //     mockStoryDataService.getStories.mockReturnValue(of(mockStories));
+
+  //     expect(spectator.service.getStories$).toBeObservable(expected);
+  //     spectator.service.getStories$.subscribe(() => {
+  //       expect(mockStoryDataService.getStories()).toHaveBeenCalled();
+  //     });
+  //   });
+  //   it('should trigger getStoriesFailure action if the call to the userDataService fails', () => {
+  //     const mockError = { message: 'Error' };
+  //     mockStoryDataService.getStories.mockReturnValue(
+  //       throwError(() => mockError),
+  //     );
+  //     mockActions$ = hot('-a', { a: AppActions.getStories() });
+  //     const expected = cold('-(a|)', {
+  //       a: AppActions.getStoriesFailure(mockError.message),
+  //     });
+
+  //     expect(spectator.service.getStories$).toBeObservable(expected);
+  //   });
+  // });
+
+  // describe('notification$', () => {
+  //   it('should call on the logService to log user and action', () => {
+  //     const expected = { user: null, action: AppActions.loadApp };
+  //     spectator.service.notification$.subscribe(() => {
+  //       expect(mockLoggerService.log()).toHaveBeenCalledWith(
+  //         expected.user,
+  //         expected.action,
+  //       );
+  //     });
+
+  //     mockStore.overrideSelector(AppFeature.selectActiveUser, null);
+  //     mockActions$ = hot('-a', { a: expected.action() });
+  //   });
+  // });
 });
